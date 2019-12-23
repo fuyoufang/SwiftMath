699f366c6eb9d795b605a1879a4ba8138272f682
fuyoufang <fuyoufang@163.com>
2019-12-23 15:13:07 +0800

    Initial Commit

---
+++ SwiftMath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMath
+//
+//  Created by 付有芳 on 2019/12/23.
+//  Copyright © 2019 付有芳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftMathTests/SwiftMathTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftMathTests.swift
+//  SwiftMathTests
+//
+//  Created by 付有芳 on 2019/12/23.
+//  Copyright © 2019 付有芳. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftMath
+
+class SwiftMathTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
